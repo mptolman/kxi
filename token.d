55b298323d6258bb88932e6ddcadd50a9bd97402
Matthew Tolman <matt@mptolman.net>
2013-12-31 08:55:32 -0700

    gitignore

--- token.d
+++ token.d
@@ -1,9 +1,43 @@
-enum TokenType
+enum TokType
 {
-	
+	INTEGER,
+	CHARACTER,
+	IDENTIFIER,
+	PUNCTUATION,
+	KEYWORD,
+	MATH_OP,
+	REL_OP,
+	BOOL_OP,
+	ASSIGN_OP,
+	ARRAY_BEGIN,
+	ARRAY_END,
+	BLOCK_BEGIN,
+	BLOCK_END,
+	PAREN_OPEN,
+	PAREN_CLOSE,
+	UNKNOWN,
+	EOT,
+	EOF
 }
 
-struct Token
+struct Tok
 {
-	string value;
+	TokType type;
+	string lexeme;
+}
+
+private:
+immutable TokRegex[] rules;
+
+struct TokRegex
+{
+	TokType type;
+	string regex;
+}
+
+static this()
+{
+	rules = [
+		TokRegex(TokType.CHARACTER, r"[a-zA-Z]")
+	];
 }
